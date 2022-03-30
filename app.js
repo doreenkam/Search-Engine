23bf9208ba028536c90bffed10a34202a1c65331
doreenkam <dkamushinda@gmail.com>
2022-03-30 17:17:34 +0100

    Added mic button

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+const url = `http://api.serpstack.com/search?access_key=${API_KEY}&query=${query}`;
+
 // index route
 app.get("/", (req, res) => {
+  res.send("Welcome to our search engine!");
+});
+
+app.get(url, (req, res) => {
   res.send("Welcome to our search engine!");
 });
 
